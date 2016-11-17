940741c37ea83335fc52b00b7ec4951b00bc8b04
Gareth Paul Jones <gareth@garethpaul.com>
2016-11-16 21:52:51 -0800

    adding ignore updates

---
+++ app/src/main/java/com/foursquare/rideup/MainActivity.java
@@ -0,0 +1,303 @@
+package com.foursquare.rideup;
+
+
+
+
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.ObjectAnimator;
+import android.animation.TypeEvaluator;
+import android.animation.ValueAnimator;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Handler;
+import android.support.annotation.DrawableRes;
+import android.support.annotation.NonNull;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.view.ContextThemeWrapper;
+import android.util.Log;
+import android.view.View;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.foursquare.api.types.Venue;
+import com.foursquare.placepicker.PlacePicker;
+import com.foursquare.placepicker.PlacePickerSdk;
+import com.mapbox.mapboxsdk.MapboxAccountManager;
+import com.mapbox.mapboxsdk.annotations.Icon;
+import com.mapbox.mapboxsdk.annotations.IconFactory;
+import com.mapbox.mapboxsdk.annotations.Marker;
+import com.mapbox.mapboxsdk.annotations.MarkerView;
+import com.mapbox.mapboxsdk.annotations.MarkerViewOptions;
+import com.mapbox.mapboxsdk.camera.CameraPosition;
+import com.mapbox.mapboxsdk.camera.CameraUpdateFactory;
+import com.mapbox.mapboxsdk.geometry.LatLng;
+import com.mapbox.mapboxsdk.geometry.LatLngBounds;
+import com.mapbox.mapboxsdk.location.LocationServices;
+import com.mapbox.mapboxsdk.maps.MapView;
+import com.mapbox.mapboxsdk.maps.MapboxMap;
+import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
+import java.util.Random;
+
+
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    private TextView pickupLocation;
+
+    private MapView mapView;
+    private MapboxMap mapboxMap;
+    private float lat;
+    private float lng;
+    private LocationServices locationServices;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+        getSupportActionBar().setCustomView(R.layout.action_bar_custom);
+        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(Color.parseColor("#FAFAFA")));
+
+        MapboxAccountManager.start(this, Constants.MAPBOX_ACCESS_TOKEN);
+        setContentView(R.layout.activity_main);
+
+        // Request a ride
+        Button confirmBtn = (Button) findViewById(R.id.confirmBtn);
+        confirmBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.v(TAG, "onclick of request ride");
+                requestRide();
+            }
+        });
+
+
+        pickupLocation = (TextView) findViewById(R.id.pickUpTextView);
+        pickupLocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                pickPlace();
+            }
+        });
+
+        PlacePickerSdk.with(new PlacePickerSdk.Builder(this)
+                .consumer(Constants.FOURSQUARE_CLIENT_KEY,
+                          Constants.FOURSQUARE_CLIENT_SECRET)
+                .imageLoader(new PlacePickerSdk.ImageLoader() {
+                    @Override
+                    public void loadImage(Context context, ImageView v, String url) {
+                        Glide.with(context)
+                                .load(url)
+                                .placeholder(R.drawable.category_none)
+                                .dontAnimate()
+                                .into(v);
+                    }
+                })
+                .build());
+
+        locationServices = LocationServices.getLocationServices(MainActivity.this);
+
+        mapView = (MapView) findViewById(R.id.mapView);
+        mapView.onCreate(savedInstanceState);
+
+
+        getClosestPlace();
+    }
+
+    private void requestRide() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(new ContextThemeWrapper(this, R.style.AlertDialogCustom));
+
+        builder.setMessage(R.string.driver_error)
+                .setTitle("Error");
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
+    private void pickPlace() {
+        Intent intent = new Intent(this, PlacePicker.class);
+        String TAG = PlacePicker.class.getSimpleName();
+        intent.putExtra(TAG + ".EXTRA_HEADER_BACKGROUND_RESOURCE", R.color.colorPrimary);
+        startActivityForResult(intent, 9001);
+    }
+
+    private void getClosestPlace() {
+        PlacePickerSdk.get().getCurrentPlace(new PlacePickerSdk.CurrentPlaceResult() {
+            @Override
+            public void success(Venue venue, boolean confident) {
+                lat = venue.getLocation().getLat();
+                lng = venue.getLocation().getLng();
+
+
+                mapView.getMapAsync(new OnMapReadyCallback() {
+
+                    @Override
+                    public void onMapReady(@NonNull final MapboxMap mapboxMap) {
+                        MainActivity.this.mapboxMap = mapboxMap;
+                        mapboxMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(lat, lng), 15));
+                        mapboxMap.setMyLocationEnabled(true);
+                        final Handler handler = new Handler();
+                        handler.postDelayed(new Runnable() {
+                            @Override
+                            public void run() {
+                                for (int i = 0; i < 10; i++) {
+                                    addRandomCar();
+                                }
+                            }
+                        }, 500);
+
+                    } // End onMapReady
+                });
+            }
+            @Override
+            public void fail() {
+            }
+        });
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == PlacePicker.PLACE_PICKED_RESULT_CODE) {
+            Venue place = data.getParcelableExtra(PlacePicker.EXTRA_PLACE);
+            pickupLocation.setText(place.getName());
+            mapboxMap.clear();
+            mapboxMap.addMarker(new MarkerViewOptions()
+                    .position(new LatLng(place.getLocation().getLat(), place.getLocation().getLng()))
+                    .title("Pick Up Location"));
+
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        mapView.onDestroy();
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mapView.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
+
+
+    protected void addRandomCar() {
+        Log.v(TAG, "addingRandomCar");
+        MarkerView car = createCarMarker(getLatLngInBounds(), R.drawable.ic_car_top);
+        randomlyMoveMarker(car);
+    }
+
+    private void randomlyMoveMarker(final MarkerView marker) {
+        Log.v(TAG, "randomlyMoveMarker");
+        ValueAnimator animator = animateMoveMarker(marker, getLatLngInBounds());
+
+        //Add listener to restart animation on end
+        animator.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                randomlyMoveMarker(marker);
+            }
+        });
+    }
+
+    private ValueAnimator animateMoveMarker(final MarkerView marker, LatLng to) {
+        marker.setRotation((float) getBearing(marker.getPosition(), to));
+
+        final ValueAnimator markerAnimator = ObjectAnimator.ofObject(
+                marker, "position", new LatLngEvaluator(), marker.getPosition(), to);
+        markerAnimator.setDuration((long) (20 * marker.getPosition().distanceTo(to)));
+        markerAnimator.setInterpolator(new AccelerateDecelerateInterpolator());
+
+        // Start
+        markerAnimator.start();
+
+        return markerAnimator;
+    }
+
+    private MarkerView createCarMarker(LatLng start, @DrawableRes int carResource) {
+        Icon icon = IconFactory.getInstance(MainActivity.this)
+                .fromResource(carResource);
+
+        Log.v(TAG, start.toString());
+        //View Markers
+        return mapboxMap.addMarker(new MarkerViewOptions()
+                .position(start)
+                .icon(icon));
+
+    }
+
+
+    private LatLng getLatLngInBounds() {
+        LatLngBounds bounds = mapboxMap.getProjection().getVisibleRegion().latLngBounds;
+        Random generator = new Random();
+        double randomLat = bounds.getLatSouth() + generator.nextDouble()
+                * (bounds.getLatNorth() - bounds.getLatSouth());
+        double randomLon = bounds.getLonWest() + generator.nextDouble()
+                * (bounds.getLonEast() - bounds.getLonWest());
+        return new LatLng(randomLat, randomLon);
+    }
+
+    /**
+     * Evaluator for LatLng pairs
+     */
+    private static class LatLngEvaluator implements TypeEvaluator<LatLng> {
+
+        private LatLng latLng = new LatLng();
+
+        @Override
+        public LatLng evaluate(float fraction, LatLng startValue, LatLng endValue) {
+            latLng.setLatitude(startValue.getLatitude()
+                    + ((endValue.getLatitude() - startValue.getLatitude()) * fraction));
+            latLng.setLongitude(startValue.getLongitude()
+                    + ((endValue.getLongitude() - startValue.getLongitude()) * fraction));
+            return latLng;
+        }
+    }
+
+    private double getBearing(LatLng from, LatLng to) {
+        double degrees2radians = Math.PI / 180;
+        double radians2degrees = 180 / Math.PI;
+
+        double lon1 = degrees2radians * from.getLongitude();
+        double lon2 = degrees2radians * to.getLongitude();
+        double lat1 = degrees2radians * from.getLatitude();
+        double lat2 = degrees2radians * to.getLatitude();
+        double a = Math.sin(lon2 - lon1) * Math.cos(lat2);
+        double b = Math.cos(lat1) * Math.sin(lat2)
+                - Math.sin(lat1) * Math.cos(lat2) * Math.cos(lon2 - lon1);
+
+        return radians2degrees * Math.atan2(a, b);
+    }
+}
